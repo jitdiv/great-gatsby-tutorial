d48c1cdddd37cb5161c6843993c392d169702647
JTDV <jtdv.info@gmail.com>
2022-10-25 12:05:47 -0700

    Set up scss for project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,5 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
-module.exports = {
-  /* Your site config here */
-  plugins: [],
+module.exports =  {
+  plugins: [
+    'gatsby-plugin-sass'
+  ],
 }
