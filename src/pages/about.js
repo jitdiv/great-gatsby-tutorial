2e4fefad34ee52f054be32810e699827c8fb4331
JTDV <jtdv.info@gmail.com>
2022-10-25 10:28:12 -0700

    Created footer component and added to each page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../components/footer'
 
 const AboutPage = () => {
   return (
     <div>
       <h1>About</h1>
       <p>This is information about site</p>
+      <Footer />
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../components/footer'
 
 const ContactPage = () => {
   return (
     <div>
       <h1>Contact</h1>
       <p>This is how to contact us: info@sample.com</p>
+      <Footer />
     </div>
   )
 }
