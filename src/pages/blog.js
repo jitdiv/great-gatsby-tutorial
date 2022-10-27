1a46e60171f01cf7031d5f92e63d5933a9b2b524
JTDV <jtdv.info@gmail.com>
2022-10-27 12:04:45 -0700

    Used graphQL query data to render title and date for each blog post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,55 @@
 import React from 'react';
-import Layout from '../components/layout'
+import Layout from '../components/layout';
+import { graphql, useStaticQuery } from 'gatsby';
 
 const BlogPage = () => {
+  const {
+    allMarkdownRemark: {
+      edges
+    }
+  } = useStaticQuery(graphql`
+  query {
+    allMarkdownRemark {
+       edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          html
+          excerpt
+        }
+      }
+    }
+  }
+  `)
+
   return (
     <div>
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here</p>
+      <p>Posts will show up here:</p>
+      <ol>
+        {
+          edges.map((post) => {
+            const {
+             node: {
+               frontmatter: {
+                 title,
+                 date
+               }
+             }
+            } = post
+
+            return (
+              <li key={String(Math.random() * 1)}>
+                <h2>{title}</h2>
+                <p>{date}</p>
+              </li>
+            )
+          })
+        }
+      </ol>
     </Layout>
     </div>
   )
